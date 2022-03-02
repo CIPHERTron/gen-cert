5356d6e6486b8b5acbe265222f4dc6a20f1203b8
CIPHERTron <pritish.samal918@gmail.com>
2022-03-02 23:09:14 +0530

    feat: add navigation bar (responsive)

---
+++ src/components/layout/Navigation.tsx
@@ -0,0 +1,60 @@
+import { Flex, Text, useColorMode } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+
+type NavItemProps = {
+  href: string;
+  label: string;
+};
+
+const NavItem = ({ href, label }: NavItemProps) => {
+  const router = useRouter();
+  const { colorMode } = useColorMode();
+
+  const handleClickNavigation = () => {
+    router.push(href);
+  };
+
+  return (
+    <Text
+      cursor="pointer"
+      margin="auto 32px"
+      fontWeight="bold"
+      onClick={handleClickNavigation}
+      color="#808080"
+      _hover={{ color: colorMode === "dark" ? "#fff" : "#000" }}
+    >
+      {label}
+    </Text>
+  );
+};
+
+const navigations: NavItemProps[] = [
+  {
+    href: "/",
+    label: "Home",
+  },
+  {
+    href: "/",
+    label: "Docs",
+  },
+  {
+    href: "/",
+    label: "Posts",
+  },
+  {
+    href: "/",
+    label: "Dashboard",
+  },
+];
+
+const Navigation = () => {
+  return (
+    <Flex display={["none", "none", "flex"]}>
+      {navigations.map((navigation) => (
+        <NavItem {...navigation} key={navigation.label} />
+      ))}
+    </Flex>
+  );
+};
+
+export default Navigation;
