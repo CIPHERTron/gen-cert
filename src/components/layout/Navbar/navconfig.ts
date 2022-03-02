bab46ef670a00f4fe8de54d6c7b8dc89feeb4dd4
CIPHERTron <pritish.samal918@gmail.com>
2022-03-02 23:09:14 +0530

    feat: add navigation bar (responsive)

---
+++ src/components/layout/Navbar/navconfig.ts
@@ -0,0 +1,22 @@
+export const navigations = [
+  {
+    href: "/",
+    label: "Home",
+  },
+  {
+    href: "/",
+    label: "Docs",
+  },
+  {
+    href: "/",
+    label: "Posts",
+  },
+  {
+    href: "/",
+    label: "Dashboard",
+  },
+  {
+    href: "/",
+    label: "Contributions",
+  },
+];
