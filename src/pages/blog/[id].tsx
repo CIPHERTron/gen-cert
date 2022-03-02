0f39c4cfa18e4dab47c9125e5813a44f541d0cd4
CIPHERTron <pritish.samal918@gmail.com>
2022-03-02 23:48:56 +0530

    feat: setup dynamic routing for docs

---
+++ src/pages/blog/[id].tsx
@@ -0,0 +1,7 @@
+import BlogPost from "components/screens/blog/post";
+
+export {
+  getStaticPaths,
+  getStaticProps,
+} from "components/screens/blog/post/loader";
+export default BlogPost;
---
+++ src/pages/blog/index.tsx
@@ -0,0 +1,4 @@
+import BlogPostList from "components/screens/blog/list";
+
+export { getStaticProps } from "components/screens/blog/list/loader";
+export default BlogPostList;
